400ce48d684baef183b3bdc58752793bc9e3a1f4
Peter Padrok <peter@padrok.de>
2025-01-03 21:43:06 +0100

    renames & submissionpage

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
 console.log("Klappt")
+
+function tosubmissionpage() {
+  console.log("called");
+  document.getElementById("bod").innerHTML='<object type="text/html" data="pages/submit.html" ></object>';
+}
