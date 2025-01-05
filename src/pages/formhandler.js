7f4b7295c72671b74e1d3d544a166a90e92f97e3
Gjergj <gjergj.hondozi@outlook.de>
2025-01-05 15:32:57 +0000

    captcha in mapge eingeflegt

--- src/pages/formhandler.js
+++ src/pages/formhandler.js
@@ -1,6 +1,50 @@
 const form = document.getElementById('upfile')
 
-form.addEventListener('submit', (event) => {
+let possible = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";  
+var captcha = "";
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+function myFunction() {
+  document.getElementById("bar").value = "";
+  captcha = "";
+  let len = Math.floor(Math.random() * 3) + 5;
+                    
+  for (let i = 0; i < len; i++){
+    let n = Math.floor(Math.random() * 62);
+    captcha += possible.charAt(n);
+  }
+                
+  document.getElementById("captcha_string").innerHTML = captcha;
+}
+
+function compareto(){
+  if (captcha == document.getElementById("bar").value){
+    modal.style.display = "none";
+    send_to_heaven();
+  } else {
+    myFunction();
+  }
+}
+
+function send_to_heaven() {
   console.log("submitted file :O")
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  modal.style.display = "block";
+  myFunction()
   event.preventDefault();
 })
